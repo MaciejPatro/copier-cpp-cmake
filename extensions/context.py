85f338c0af41cac461d17be310d759bd32948f23
Maciej Patro <maciejpatro@gmail.com>
2022-06-13 14:10:32 +0200

    Move year handling to context extensions

--- extensions/context.py
+++ extensions/context.py
@@ -1,3 +1,4 @@
+import datetime
 from copier_templates_extensions import ContextHook
 
 
@@ -9,6 +10,9 @@ class ContextUpdater(ContextHook):
             context['unit_testing'] = context['unit_testing_98']
         else:
             context['unit_testing'] = context['unit_testing_11']
+
+        if context['license'] != 'None':
+            context['creation_year'] = datetime.date.today().strftime("%Y")
 
         context['dependencies'] = context['unit_testing'] != 'None'
         context['has_main'] = 'executable' in context['project_type']
