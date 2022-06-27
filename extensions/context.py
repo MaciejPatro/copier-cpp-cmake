f899f9b2efa7121bbe0000948e373dd749a72136
Maciej Patro <maciejpatro@gmail.com>
2022-06-27 15:03:32 +0200

    Add support for Catch2/boost-ext-ut

--- extensions/context.py
+++ extensions/context.py
@@ -6,10 +6,14 @@ class ContextUpdater(ContextHook):
     update = False
 
     def hook(self, context):
-        if context['unit_testing_98']:
+        if context['cpp_standard'] == 0:
             context['unit_testing'] = context['unit_testing_98']
-        else:
+        elif context['cpp_standard'] == 11:
             context['unit_testing'] = context['unit_testing_11']
+        elif context['cpp_standard'] == 14:
+            context['unit_testing'] = context['unit_testing_14']
+        else:
+            context['unit_testing'] = context['unit_testing_17']
 
         if context['license'] != 'None':
             context['creation_year'] = datetime.date.today().strftime("%Y")
